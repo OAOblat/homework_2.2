25c8916fa4ab70cc85f10f741daedd1f881adfc9
Olga Oblat <oa.oblat@gmail.com>
2023-07-26 21:49:11 +0300

    доработка

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -11,10 +11,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-
 
 
 class CardDeliveryTest {
